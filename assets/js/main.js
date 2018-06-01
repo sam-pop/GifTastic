31fb99383d73805dd8ec43268a532c00bd78d7f3
sam-pop <poplovitch.samuel@gmail.com>
2018-05-31 23:54:44 -0400

    comments added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,10 @@
 var apiKey = "yLcI2YzFd7HS2iXNcVmAYoktCbwK6U9b"; // giphy api key
 var limit = 10; // the number of returned results
-var searchParam;
+var searchParam; // holds the query parameter
 
 var topics = ["Jerry Seinfeld", "George Costanza", "Elaine Benes", "Cosmo Kramer", "Newman", "Frank Costanza", "Estelle Costanza"]; // button array
 
+// creates the buttons from the topics array and appends them to the page
 function populateButtons() {
     $('.displayBtns').empty();
     topics.forEach(function (item) {
@@ -14,17 +15,17 @@ function populateButtons() {
     });
 }
 
+// creates the page content (cards holding gifs and rating) from the api response
 function populateMain(res) {
     var main = $('.main');
 
     main.empty();
-
     for (var i = 0; i < res.data.length; i++) {
         var rating = res.data[i].rating;
         var img = $('<img>').attr({
             'src': res.data[i].images.fixed_height_still.url,
-            'data-still': res.data[i].images.fixed_height_still.url,
-            'data-live': res.data[i].images.fixed_height_downsampled.url
+            'data-still': res.data[i].images.fixed_height_still.url, // saves the still image src
+            'data-live': res.data[i].images.fixed_height_downsampled.url // saves the live image src
         }).addClass('gifImg responsive-img');
         if (rating && img) {
             var card = $('<div>').addClass('card center');
@@ -38,6 +39,7 @@ function populateMain(res) {
     }
 }
 
+// on click adds the current search field value to the array and recreates the buttons on the screen
 $('#submitBtn').click(function (e) {
     e.preventDefault();
     var inputText = $('#searchField').val().trim();
@@ -47,27 +49,28 @@ $('#submitBtn').click(function (e) {
     }
 });
 
+// on click updates the search parameter and runs the API query
 $(document).on('click', '.arrBtn', function () {
     searchParam = this.textContent.replace(/ /g, "+");
     runAPI();
 });
 
+// on click changes the image source between live and still images
 $(document).on('click', '.gifImg', function () {
-
     if ($(this).attr('src') == $(this).attr('data-still')) {
         $(this).attr('src', $(this).attr('data-live'));
     } else $(this).attr('src', $(this).attr('data-still'));
-
 });
 
+
+// fetches json from the giphy API
 function runAPI() {
     var urlAPI = "http://api.giphy.com/v1/gifs/search?q=" + searchParam + "&api_key=" + apiKey + "&limit=" + limit;
     $.ajax({
         url: urlAPI,
         type: "GET",
         success: function (res) {
-            populateMain(res);
-
+            populateMain(res); // creates the main content of the page
         },
         error: function () {
             console.log("API ERROR");
@@ -76,5 +79,5 @@ function runAPI() {
 }
 
 $(function () {
-    populateButtons();
+    populateButtons(); // creates the initial buttons on document ready
 });
