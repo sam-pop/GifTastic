60d33c153457e36f31bbb6aba124e826ec3464c2
sam-pop <poplovitch.samuel@gmail.com>
2018-05-31 22:38:30 -0400

    js update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 var apiKey = "yLcI2YzFd7HS2iXNcVmAYoktCbwK6U9b"; // giphy api key
-var limit = 2; // the number of returned results
+var limit = 10; // the number of returned results
 var searchParam;
 
 var buttons = ["Jerry Seinfeld", "George Costanza", "Elaine Benes", "Cosmo Kramer", "Newman"]; // button array
@@ -15,7 +15,21 @@ function populateButtons() {
 }
 
 function populateMain(res) {
+    var main = $('.main');
 
+    main.empty();
+
+    for (var i = 0; i < res.data.length; i++) {
+        var rating = res.rating;
+        var img = $('<img>').attr({
+            'src': res.data[i].images.fixed_height_still.url,
+            'data-still': res.data[i].images.fixed_height_still.url,
+            'data-live': res.data[i].images.fixed_height_downsampled.url
+        }).addClass('gifImg responsive-img ');
+        main.append(res.data[i].rating);
+        main.append(img);
+        main.append("&nbsp;&nbsp;");
+    }
 }
 
 $('#submitBtn').click(function (e) {
@@ -30,6 +44,14 @@ $('#submitBtn').click(function (e) {
 $(document).on('click', '.arrBtn', function () {
     searchParam = this.textContent.replace(/ /g, "+");
     runAPI();
+});
+
+$(document).on('click', '.gifImg', function () {
+
+    if ($(this).attr('src') == $(this).attr('data-still')) {
+        $(this).attr('src', $(this).attr('data-live'));
+    } else $(this).attr('src', $(this).attr('data-still'));
+
 });
 
 function runAPI() {
