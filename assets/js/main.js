fe47e7aacd53682327df3edfd003b333a38369c4
sam-pop <poplovitch.samuel@gmail.com>
2018-05-31 22:11:52 -0400

    js and html update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,52 @@
-var limit = 10; // the number of returned results
-var searchParam = "swanson"; //TODO: initialize
 var apiKey = "yLcI2YzFd7HS2iXNcVmAYoktCbwK6U9b"; // giphy api key
+var limit = 2; // the number of returned results
+var searchParam;
 
-var urlAPI = "http://api.giphy.com/v1/gifs/search?q=" + searchParam + "&api_key=" + apiKey + "&limit=" + limit;
+var buttons = ["Jerry Seinfeld", "George Costanza", "Elaine Benes", "Cosmo Kramer", "Newman"]; // button array
+
+function populateButtons() {
+    $('.displayBtns').empty();
+    buttons.forEach(function (item) {
+        var btn = $('<button>').addClass('btn red arrBtn');
+        btn.text(item);
+        $('.displayBtns').append(btn);
+        $('.displayBtns').append("<br><br>");
+    });
+}
+
+function populateMain(res) {
+
+}
+
+$('#submitBtn').click(function (e) {
+    e.preventDefault();
+    var inputText = $('#searchField').val().trim();
+    if (buttons.indexOf(inputText) == -1) {
+        buttons.push(inputText);
+        populateButtons();
+    }
+});
+
+$(document).on('click', '.arrBtn', function () {
+    searchParam = this.textContent.replace(/ /g, "+");
+    runAPI();
+});
+
+function runAPI() {
+    var urlAPI = "http://api.giphy.com/v1/gifs/search?q=" + searchParam + "&api_key=" + apiKey + "&limit=" + limit;
+    $.ajax({
+        url: urlAPI,
+        type: "GET",
+        success: function (res) {
+            populateMain(res);
+
+        },
+        error: function () {
+            console.log("API ERROR");
+        }
+    });
+}
+
+$(function () {
+    populateButtons();
+});
